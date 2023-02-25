fb7b4734114420738088652daf56cff8e3993027
mfrugoni <miguelmfrugoni@gmail.com>
2023-02-24 23:20:40 -0300

    Version 12: cargar la data y codificar los archivos js para lograr la carga dinamica de las cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,27 @@
 console.log("Index");
+
+//Function that creates the card templates in string format:
+function createCards(dataArray) {
+    let cardString = ``;
+
+    for (let event of dataArray) {
+        cardString += `<div class="card">
+    <img src="${event.image}">
+    <h3>${event.name}</h3>
+    <p>Date: ${event.date}</p>
+    <p>Description: ${event.description} </p>
+    <div class="card-foot">
+        <p>Price: ${event.price}</p>
+        <a href="./details.html">More...</a>
+    </div>
+</div>
+`
+    }
+    return cardString;
+}
+
+// console.log(createCards(data.events));
+//Capture the html element where we want to put the cards:
+const box = document.getElementById("box");
+//Create the cards and put them inside the captured element:
+box.innerHTML = createCards(data.events);
--- scripts/upcoming-events.js
+++ scripts/upcoming-events.js
@@ -1,1 +1,47 @@
 console.log("Upcoming Events");
+
+//Declaration of the arrays where past and future events will be stored:
+let pastEvents = [];
+let futureEvents = [];
+
+//Function that filters past and future events based on currentDate:
+function filterEvents(dataArray) {
+
+    for (let event of dataArray) {
+        if (event.date < data.currentDate) {
+            pastEvents.push(event);
+        }
+        else if (event.date >= data.currentDate) {
+            futureEvents.push(event);
+        }
+    }
+}
+
+//Function that creates the card templates in string format:
+function createCards(dataArray) {
+    let cardString = ``;
+    for (let event of dataArray) {
+        cardString += `<div class="card">
+    <img src="${event.image}">
+    <h3>${event.name}</h3>
+    <p>Date: ${event.date}</p>
+    <p>Description: ${event.description} </p>
+    <div class="card-foot">
+        <p>Price: ${event.price}</p>
+        <a href="./details.html">More...</a>
+    </div>
+</div>
+`
+    }
+    return cardString;
+}
+
+//Function call to filter the events:
+filterEvents(data.events);
+// console.log(futureEvents)
+//Capture the html element where we want to put the cards:
+const box = document.getElementById("box");
+//Create the cards and put them inside the captured element:
+box.innerHTML = createCards(futureEvents);
+
+
