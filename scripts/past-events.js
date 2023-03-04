36acdbab8b0ea43555fdc16f26b3ae32f539d4e2
mfrugoni <miguelmfrugoni@gmail.com>
2023-03-04 20:04:33 -0300

    Version 18: lo mismo en upcoming y past events

--- scripts/past-events.js
+++ scripts/past-events.js
@@ -44,3 +44,42 @@ filterEvents(data.events);
 const box = document.getElementById("box");
 //Create the cards and put them inside the captured element:
 box.innerHTML = createCards(pastEvents);
+
+//----Categories----//
+//Creates an array of categories iterating data.events:
+let arrCategory = [];
+
+for(let event of data.events){
+  arrCategory.push(event.category)
+}
+console.log(`arrCategory ${arrCategory}`);
+//Iterates the categories array and saves the non-repeating categories in a new array:
+let aECategoriesArray = [];
+
+arrCategory.forEach(category =>{
+  if (!aECategoriesArray.includes(category)){
+    aECategoriesArray.push(category)
+  }
+})
+//Sorts the resulting array
+aECategoriesArray.sort()
+
+console.log(`aECategoriesArray:  ${aECategoriesArray}`)
+
+//Creates the checkboxes by passing the resulting array to the function create:
+function createCategoriesCheckBox(categoriesArray){
+    let categoriesString = "";
+    for (const category of categoriesArray){
+        categoriesString += `
+    <div>
+        <input type="checkbox" name="category" id="${category.toLowerCase()}" value="${category.toLowerCase()}">
+        <label for="${category.toLowerCase()}">${category}</label>
+    </div> `
+    }
+    return categoriesString;
+}
+
+const form = document.querySelector(".category");
+console.log(form);
+form.innerHTML = createCategoriesCheckBox(aECategoriesArray);
+console.log(createCategoriesCheckBox(aECategoriesArray));
